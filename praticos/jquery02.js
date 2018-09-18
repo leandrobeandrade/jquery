5bc91b359ee4be830f4464b0039cfe81f626240a
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-09-18 17:35:06 -0300

    jquery 02

--- praticos/jquery02.js
+++ praticos/jquery02.js
@@ -1,11 +1,11 @@
 $(document).ajaxSuccess(function() {
 	$(".log").text("OUTRA PÁGINA CARREGADA VIA AJAX");
-});																					// ajax()
+});															// ajax()
 
 $(document).ajaxSend(function( event, request, settings) {
 	$(".msg").append("<li>Realizou a requisição em " + settings.url + "</li><br>");
 });
 
-$("button").click(function() {														// click()
-  	$(".result").load("jquery01.html #other-page");												// load()
+$("button").click(function() {												// click()
+  	$(".result").load("jquery01.html #other-page");									// load()
 });
