5e5048ea8df044911ac6b82ed3eaf636169332e2
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-09-24 18:26:28 -0300

    jquery 07

--- praticos/jquery07.js
+++ praticos/jquery07.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-	$(".li").each(function(index) {														// each()
+	$(".li").each(function(index) {										// each()
 		$("span").text(index + ": " + $(".li").text());
 	});
 
-	$(".um").empty();																	// empty()
+	$(".um").empty();											// empty()
 
 	$("strong:empty").text("Estava vazio, foi selecionado com :empty");					// :empty
 
 	$("input:disabled").val("Input desabilitado!");								// :disabled
 
-	$("ul.first").find(".foo")															// find()
+	$("ul.first").find(".foo")										// find()
 	.css("background-color", "red")
 	.end()
 	.find(".bar")
 	.css("background-color", "green")
-	.end();																				// end()
+	.end();													// end()
 
 	$(".div1").eq(3).css("background-color", "blue");							// eq()
 	$(".div1").eq(-2).css("background-color", "black");
