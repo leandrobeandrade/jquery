857cb506889cfe886604e4150c963426e0169327
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-09-29 15:46:50 -0300

    jquery 10

--- praticos/jquery10.js
+++ praticos/jquery10.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
 	$("#a").click(function() {
-		$("#div1").fadeIn(3000, function() {										// fadeIn()
+		$("#div1").fadeIn(3000, function() {									// fadeIn()
 			$("#strong1").fadeIn(100);
 		});
 		return false;
 	});
 
 	$(".span").click(function() {
-		$(this).fadeOut(1000, function() {											// fadeOut()
+		$(this).fadeOut(1000, function() {									// fadeOut()
 			$("#div2").text("'" + $(this).text() + "' foi faded!");					
-			$(this).remove();														// remove()
+			$(this).remove();										// remove()
 		});
 	});
 	$(".span").hover(function() {													
-		$(this).addClass("hilite");													// addClass()
+		$(this).addClass("hilite");										// addClass()
 	}, function() {
-		$(this).removeClass("hilite");												// removeClass()
+		$(this).removeClass("hilite");										// removeClass()
 	});
 
 	$(".div").click(function() {
-		$(this).fadeTo("fast", Math.random());										// fadeTo()
+		$(this).fadeTo("fast", Math.random());									// fadeTo()
 	});
 
-	$("button:first").click(function() {											// :first
-		$("b:first").fadeToggle("slow", "linear");									// fadeToggle()
+	$("button:first").click(function() {										// :first
+		$("b:first").fadeToggle("slow", "linear");								// fadeToggle()
 	});
-	$("button:last").click(function() {												// :last
+	$("button:last").click(function() {										// :last
 		$("b:last").fadeToggle("fast", function() {
 			$("#log").append("<i>finalizado </i>");
 		});
 	});
 
-	$(".li1").filter(":even").css({													// filter()
+	$(".li1").filter(":even").css({											// filter()
 		"background-color": "red",
 		"color": "#fff"
 	});
 
 	$(".li2").filter(function(index) {
 		return $("em", this).length === 1;
-	}).css({																		// css()
+	}).css({													// css()
 		"background-color": "red",
 		"color": "#fff"
 	});
