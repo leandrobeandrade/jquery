237a86ac360f8dadbb60a252b77cb94c3bdc7ec7
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-10-10 15:30:24 -0300

    jquery 16

--- praticos/jquery16.js
+++ praticos/jquery16.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	var str = "         neste texto encontram-se espaços no começo e no final        ";
 	$("#original").html("Original String: '" + str + "'");
-	$("#trimmed").html("Agora depois do trim(): '" + $.trim(str) + "'");				// $.trim()
+	$("#trimmed").html("Agora depois do trim(): '" + $.trim(str) + "'");					// $.trim()
 
-	var d1 = $.Deferred();																// $.Deferred()
+	var d1 = $.Deferred();											// $.Deferred()
 	var d2 = $.Deferred();
 	var d3 = $.Deferred();
 
-	$.when(d1, d2, d3).done(function (v1, v2, v3) {										// $.when()
+	$.when(d1, d2, d3).done(function (v1, v2, v3) {								// $.when()
 		$("#span1").text(v1); // v1 is undefined
 		$("#span2").text(v2); // v2 is "abc"
 		$("#span3").text(v3); // v3 is an array [ 1, 2, 3, 4, 5 ]
@@ -17,22 +17,22 @@ $(document).ready(function() {
 	d2.resolve("abc");
 	d3.resolve(1, 2, 3, 4, 5);
 
-	$("#input1").keydown(function() {													// keydown()
+	$("#input1").keydown(function() {									// keydown()
 		var texto = $(this).val();
 		alert("Feito com keydown() - " + texto);
 	});
 
-	$("#input2").keypress(function() {													// keypress()
+	$("#input2").keypress(function() {									// keypress()
 		$("#p1").css("color","red");
 	});
 
-	$("#input3").keyup(function() {														// keyup()
+	$("#input3").keyup(function() {										// keyup()
 		$("#p2").append("Testando keyup()");
 	});
 
-	$("strong span").last().addClass("ligado");											// last()
+	$("strong span").last().addClass("ligado");								// last()
 
-	$("#div1 span:last-child")															// :last-child
+	$("#div1 span:last-child")										// :last-child
 	.css({color:"red", fontSize:"80%"})
 	.hover(function() {
 		$(this).addClass("ultimo");
@@ -40,7 +40,7 @@ $(document).ready(function() {
 		$(this).removeClass("ultimo");
 	});
 
-	$("#div2 span:last-of-type")														// :last-of-type
+	$("#div2 span:last-of-type")										// :last-of-type
 	.css({color:"red", fontSize:"80%"})
 	.hover(function() {
 		$(this).addClass("ultimo");
@@ -50,11 +50,11 @@ $(document).ready(function() {
 
 	$("#div3").on("click", function() {
 		$("#div3").append($("<div class='box'>"));
-		var n = $(".box").length;														// length
+		var n = $(".box").length;									// length
 		$("em").text("Há " + n + " divs." + " Clique para adicionar mais.");
-	}).trigger("click");																// trigger()
+	}).trigger("click");											// trigger()
 
-	$("td:lt(4)").css("backgroundColor", "yellow");										// :lt(index)
+	$("td:lt(4)").css("backgroundColor", "yellow");								// :lt(index)
 	$("td:lt(-2)").css("color", "red");
 
 })
