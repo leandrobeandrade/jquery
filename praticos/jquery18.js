6bbcfe5ac97e98c5cd673f148c95013468bda4f5
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-10-15 14:55:47 -0300

    jquery 18

--- praticos/jquery18.js
+++ praticos/jquery18.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-	$("label + input").css({															// next adjacente
+	$("label + input").css({										// next adjacente
 		"color":"blue","border":"solid 1px blue"
 	}).val("Encontrado por label + input");
 
-	$("#prev ~ .div").css("border", "3px groove blue");									// next irmãos
+	$("#prev ~ .div").css("border", "3px groove blue");							// next irmãos
 
-	$(".div1:first").nextAll().addClass("after");										// nextAll()
+	$(".div1:first").nextAll().addClass("after");								// nextAll()
 
 	$("#term-2").nextUntil("dt").css("background-color", "#ccc");						// nextUntil()
 
 	var term3 = $("#term-3");
-	$("#term-1").nextUntil(term3, "dd").css("color", "red");							// nextUntil()
+	$("#term-1").nextUntil(term3, "dd").css("color", "red");						// nextUntil()
 
 })
