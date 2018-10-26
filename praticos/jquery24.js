842f3f6cb974c14321d7d49976d1ffc234bb8a12
Leandro B Andrade <leandrobe.andrade@gmail.com>
2018-10-26 17:07:06 -0300

    jquery 24

--- praticos/jquery24.js
+++ praticos/jquery24.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-	$("div .demo").scrollLeft(300);														// scrollLeft()
+	$("div .demo").scrollLeft(300);										// scrollLeft()
 
-	$("div .mode").scrollTop(300);														// scrollTop()
+	$("div .mode").scrollTop(300);										// scrollTop()
 
 	$("textarea").select(function() {
-		var txt = $(this).val();														// select()
+		var txt = $(this).val();									// select()
 		$("code").text(txt).show().fadeOut(1000);
 	});
 
 	$("select").change(function() {
 		var str = "";
-		$("select option:selected").each(function() {									// :selected
+		$("select option:selected").each(function() {							// :selected
 			str += $(this).text() + " ";
 		});
 		$("b").text(str);
-	}).trigger("change");																// trigger()
+	}).trigger("change");											// trigger()
 
 	function showValues() {
-		var str = $("#form1").serialize();												// serialize()
+		var str = $("#form1").serialize();								// serialize()
 		$("em").text(str);
 	}
 	$("#check1, #check2").on("click", showValues);
 	showValues();
 
 	function showValue() {
-		var fields = $("#check3, #check4, #radio2").serializeArray();				// serializeArray()
+		var fields = $("#check3, #check4, #radio2").serializeArray();					// serializeArray()
 		$("h6").empty();
 		$.each(fields, function(i, field) {
 			$("h6").append(field.value + " ");
