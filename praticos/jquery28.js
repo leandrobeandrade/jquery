ee8e8577ca39ae732e8dda2e55b8d9b1d82869ee
Leandro B Andrade <leandrobe.andrade@gmail.com>
2018-11-22 15:55:27 -0200

    jquery 28

--- praticos/jquery28.js
+++ praticos/jquery28.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	var larg = $(window).width();														// width()
+	var larg = $(window).width();											// width()
 	$("h2").append(larg + "px");
 
-	$("p").wrap("<div class='div1'></div>");											// wrap()
+	$("p").wrap("<div class='div1'></div>");									// wrap()
 
-	$("h6").wrapAll("<div class='div1'></div>");										// wrapAll()
+	$("h6").wrapAll("<div class='div1'></div>");									// wrapAll()
 
-	$("#div").wrapInner("<div class='div2'><code><em></em></code></div>");				// wrapInner()
+	$("#div").wrapInner("<div class='div2'><code><em></em></code></div>");						// wrapInner()
 
 })
